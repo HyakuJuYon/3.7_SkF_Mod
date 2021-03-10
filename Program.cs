c7ced7018afd7b14367e5916f113060907718f18
Hyaku Ju Yon <ichitora114@outlook.com>
2021-03-10 04:35:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3._7_SkF_Mod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age, please: ");
+
+            byte age = (byte)int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your age is {0}", age);
+           
+            Console.ReadKey();
+        }
+    }
+}
