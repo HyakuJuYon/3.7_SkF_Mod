1cf6757c1f3a311a033a2c321095a087a2b7cc39
Hyaku Ju Yon <ichitora114@outlook.com>
2021-03-10 04:59:37 +0300

    Changes_2

--- Program.cs
+++ Program.cs
@@ -12,11 +12,27 @@ namespace _3._7_SkF_Mod
 
             Console.WriteLine("Enter your age, please: ");
 
-            byte age = checked ((byte) int.Parse(Console.ReadLine()));
+            byte age = checked((byte)int.Parse(Console.ReadLine()));
 
             Console.WriteLine("Your name is {0} and your age is {1}", Name, age);
-           
+
+            Console.WriteLine("Hwat is your favourite day of the week: ");
+
+            DayOfWeek day = (DayOfWeek) int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your favourite day of the week is: {0}", day);
+
             Console.ReadKey();
         }
+    }
+    enum DaysOfWeek : byte
+    {
+        Sunday = 1,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
     }
 }
